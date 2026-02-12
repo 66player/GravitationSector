6d2c668400f1f60c26b16dbc92626b6681d83a43
0nyc <anteq0567@gmail.com>
2026-02-12 12:25:46 +0100

    ver1

---
+++ src/main/java/zero/nyc/gravitationsector/GravitationSector.java
@@ -0,0 +1,16 @@
+package zero.nyc.gravitationsector;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GravitationSector extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
