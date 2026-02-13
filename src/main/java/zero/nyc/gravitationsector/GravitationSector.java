a0a6edcb12f1325e234fc6598dea72309966fb42
0nyc <anteq0567@gmail.com>
2026-02-13 11:40:36 +0100

    dodano troche kodu ale w chuj do zrobieia jeszcze

--- src/main/java/zero/nyc/gravitationsector/GravitationSector.java
+++ src/main/java/zero/nyc/gravitationsector/GravitationSector.java
@@ -1,43 +1,102 @@
 package zero.nyc.gravitationsector;
 
-import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.protection.managers.RegionManager;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import com.sk89q.worldguard.protection.regions.RegionContainer;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.plugin.Plugin;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public final class GravitationSector extends JavaPlugin implements Listener {
 
+    public RegionContainer container;
+
     @Override
     public void onEnable() {
+        getCommand("spacehelmet").setExecutor(new SpaceHelmet());
         getServer().getPluginManager().registerEvents(this, this);
+        container = WorldGuard.getInstance().getPlatform().getRegionContainer();
     }
 
     @Override
     public void onDisable() {
     }
 
-    private WorldGuardPlugin worldGuard() {
-        Plugin pl = this.getServer().getPluginManager().getPlugin("WorldGuard");
-        if (!(pl instanceof WorldGuardPlugin))
-        {
-            return null;
+    public static List<String> playersInSafeZone = new ArrayList<>(); //UUID Gracza
+    public static List<String> playersInSpace = new ArrayList<>(); //UUID Gracza
+    public ProtectedRegion safezone;
+    public ProtectedRegion space;
+
+    public void PlayerZone(Player player) {
+        RegionManager regions = container.get(BukkitAdapter.adapt(player.getWorld()));
+        if (regions != null) {
+            safezone = regions.getRegion("safe-zone");
+            space = regions.getRegion("zone");
+
+            int x = player.getLocation().getBlockX();
+            int y = player.getLocation().getBlockY();
+            int z = player.getLocation().getBlockZ();
+            String uuid = player.getUniqueId().toString();
+
+            if (safezone.contains(x,y,z)) {
+                playersInSafeZone.add(uuid);
+            } else if (space.contains(x,y,z)) {
+                playersInSpace.add(uuid);
+            }
+            else {
+                if (playersInSpace.contains(uuid)) {
+                    playersInSpace.remove(uuid);
+                } else if (playersInSafeZone.contains(uuid)) {
+                    playersInSafeZone.remove(uuid);
+                }
+            }
         }
-        return (WorldGuardPlugin) pl;
     }
 
-    public boolean PlayerInRegion(Player player) {
-        ProtectedRegion region = worldGuard().
-        int x = player.getLocation().getBlockX();
-        int y = player.getLocation().getBlockY();
-        int z = player.getLocation().getBlockZ();
-
-        if (region.contains(x, y, z)) {
-            return true;
-        } else {
-            return false;
+    public void addEffect(Player player, boolean isSafe) {
+        //TODO space_helmet
+        if (isSafe) {
+            player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 1, 2));
+            player.setRemainingAir(player.getMaximumAir());
+        } else if (!isSafe) {
+            player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING, 1, 2));
+            player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 1, 3));
+            if (!player.getInventory().getHelmet().getItemMeta().getPersistentDataContainer().getKeys().contains(SpaceHelmet.createHelmet().getItemMeta().getPersistentDataContainer().getKeys())) {
+                new BukkitRunnable() {
+                    public void run() {
+                        if (playersInSafeZone.contains(player.getUniqueId().toString())) {
+                            cancel();
+                            return;
+                        }
+                        int air = player.getRemainingAir();
+                        player.setRemainingAir(air - 30);
+                    }
+                }.runTaskTimer(this, 0, 20L);
+            } else {
+                player.setRemainingAir(player.getMaximumAir());
+                player.addPotionEffect(new PotionEffect(PotionEffectType.WATER_BREATHING, 2, 2));
+            }
         }
     }
-
+    @EventHandler
+    public void playerMoveEvent(PlayerMoveEvent playerMoveEvent) {
+        Player p = playerMoveEvent.getPlayer();
+        String uuid = p.getUniqueId().toString();
+        PlayerZone(p);
+        if (playersInSafeZone.contains(uuid)) {
+            addEffect(p, true);
+        } else if (playersInSpace.contains(uuid)) {
+            addEffect(p, false);
+        }
+    }
 }
---
+++ src/main/java/zero/nyc/gravitationsector/SpaceHelmet.java
@@ -0,0 +1,38 @@
+package zero.nyc.gravitationsector;
+
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.plugin.Plugin;
+
+public class SpaceHelmet implements CommandExecutor {
+
+    public static ItemStack createHelmet() {
+        ItemStack helmet = new ItemStack(Material.CHAINMAIL_HELMET);
+        ItemMeta meta = helmet.getItemMeta();
+
+        meta.setDisplayName("§bSpace Helmet");
+
+        Plugin plugin = GravitationSector.getPlugin(GravitationSector.class);
+
+        NamespacedKey key = new NamespacedKey(plugin, "space_helmet");
+        meta.getPersistentDataContainer().set(key, PersistentDataType.INTEGER, 1);
+        helmet.setItemMeta(meta);
+        return helmet;
+    }
+
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] label) {
+        if (sender instanceof Player p && sender.isOp()) {
+            p.getInventory().addItem(createHelmet());
+        }
+        return false;
+    }
+}
